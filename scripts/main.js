da1c93992f983128eeb594e06a40d82f5fe203dd
Borys Amoian <baris.003@icloud.com>
2025-05-09 18:56:26 +0200

    feat: added responsive navbar

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// Sidebar buttons
+
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar() {
+    sidebar.style.display = "none";
+}
