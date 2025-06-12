fc41eb76bf263931d28d7a38fadebab0037be3b6
Borys Amoian <baris.003@icloud.com>
2025-06-12 19:32:24 +0200

    feat: scrolling to next question in quiz

---
+++ scripts/quiz.js
@@ -0,0 +1,6 @@
+// Scrolling to the next question onclick
+function scrollToNext(sectionId) {
+    section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth', block: 'center'});
+
+}
